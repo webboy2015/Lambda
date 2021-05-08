994c1d37cebd8cf0413a904bfb61e1dcde672b73
webboy2015 <arunchau@andrew.cmu.edu>
2021-05-07 17:13:16 -0700

    Update Final-Lambda-Tested.py

--- Final-Lambda-Tested.py
+++ Final-Lambda-Tested.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 
+# This lambda function is used to revoke security group ingress rules based on cloudwatch event "AuthorizeSecurityGroupIngress" when the Security Group allows SSH to world.
 
 def lambda_handler(event, context):
     # event = {"version":"0","id":"7c5b223c-1517-4e97-c853-89497afaf3b2","detail-type":"AWS API Call via CloudTrail","source":"aws.ec2","account":"963584523464","time":"2021-05-07T17:42:22Z","region":"us-east-1","resources":[],"detail":{"eventVersion":"1.08","userIdentity":{"type":"Root","principalId":"963584523464","arn":"arn:aws:iam::963584523464:root","accountId":"963584523464","accessKeyId":"ASIA6AWQ47TEKUBOLMOH","sessionContext":{"sessionIssuer":{},"webIdFederationData":{},"attributes":{"mfaAuthenticated":"True","creationDate":"2021-05-07T17:31:52Z"}}},"eventTime":"2021-05-07T17:42:22Z","eventSource":"ec2.amazonaws.com","eventName":"AuthorizeSecurityGroupIngress","awsRegion":"us-east-1","sourceIPAddress":"165.225.243.77","userAgent":"console.ec2.amazonaws.com","requestParameters":{"groupId":"sg-0db7d4784c4db8612","ipPermissions":{"items":[{"ipProtocol":"tcp","fromPort":22,"toPort":22,"groups":{},"ipRanges":{"items":[{"cidrIp":"0.0.0.0/0"}]},"ipv6Ranges":{"items":[{"cidrIpv6":"::/0"}]},"prefixListIds":{}}]}},"responseElements":{"requestId":"dae02e73-e4c0-4318-b131-a7daa762ab1b","_return":True},"requestID":"dae02e73-e4c0-4318-b131-a7daa762ab1b","eventID":"7718d386-c058-4b0a-b6b7-6ffa2870aeaf","readOnly":False,"eventType":"AwsApiCall","managementEvent":True,"eventCategory":"Management"}}
